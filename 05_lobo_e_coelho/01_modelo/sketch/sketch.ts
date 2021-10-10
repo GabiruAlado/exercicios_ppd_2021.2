5fbe0c92161d860449554e6c2de590dc7cec0d3e
Fellipe <fmayan999@gmail.con>
2021-10-10 17:32:42 -0300

    first commit

---
+++ 05_lobo_e_coelho/01_modelo/sketch/sketch.ts
@@ -0,0 +1,17 @@
+//importar imagens
+let wolf_image: p5.Image;
+
+function pre_load() {
+  wolf_image = loadImage('../sketch/lobinho.png');
+}
+
+//setup inicializa as funções do p5
+function setup() {
+  createCanvas(500,500);
+  
+}
+
+//draw "desenha" a tela a cada frame
+function draw() {
+
+}
